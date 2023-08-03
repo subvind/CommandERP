79051db6c1043eeed784e5a9da4a1686d611a711
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-08-03 16:15:38 -0500

    erp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <div class="container" style="width: 600px;">
   <div class="card">
     <div class="card-content">
-      <h1 style="margin: 0;">Enterprise Resource Management</h1>
+      <h1 style="margin: 0;">Enterprise Resource Planning</h1>
       <p>Visit <a href="https://subvind.com">subvind.com</a> to read the documentation.</p>
       <br />
       <a href="/auth/login" class="waves-effect black white-text btn">Login</a>
