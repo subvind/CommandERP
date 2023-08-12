7c3fb9812e6ae13d2668875a0a8f0f88b0fe9874
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-08-12 15:35:46 -0500

    dropdowns

--- src/routes/organizations/+page.svelte
+++ src/routes/organizations/+page.svelte
@@ -54,8 +54,33 @@
     ]
   }
 
+  onMount(() => {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {
+      alignment: 'right'
+    });
+  })
 </script>
 
+<nav class="nav-extended yellow lighten-2">
+  <div class="container">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo black-text">Organizations</a>
+    </div>
+
+    <a href="#" data-target='dropdown1' class="btn-floating btn-large waves-effect waves-light black dropdown-trigger right" style="margin-top: -1.9em;"><i class="material-icons">more_vert</i></a>
+    <!-- Dropdown Structure -->
+    <ul id='dropdown1' class='dropdown-content'>
+      <li><a href="#!">create new</a></li>
+      <li class="divider" tabindex="-1"></li>
+      <li><a href="#!">JSON import</a></li>
+      <li><a href="#!">JSON export</a></li>
+      <li class="divider" tabindex="-1"></li>
+      <li><a href="#!">documentation</a></li>
+      <li><a href="#!">pricing</a></li>
+    </ul>
+  </div>
+</nav>
 <br />
 <br />
 <!-- {JSON.stringify(users)} -->
@@ -63,3 +88,9 @@
 <div class="container">
   <Table url={url} columns={columns} limit={limit} mapResultsFunc={mapResultsFunc} sort={sort} />
 </div>
+
+<style>
+  .dropdown-content {
+    width: 200px !important;
+  }
+</style>
