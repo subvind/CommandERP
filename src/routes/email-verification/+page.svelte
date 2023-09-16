b76060fd2f5d98330801a0af740d80e3fe1bc5fa
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-09-15 21:19:37 -0500

    verify user 2

--- src/routes/email-verification/+page.svelte
+++ src/routes/email-verification/+page.svelte
@@ -1,5 +1,66 @@
 <script lang="ts">
 	import { onMount } from "svelte";
+  import jwt_decode from 'jwt-decode';
+
+  let decodedToken: any = null;
+  let user: any;
+	let loading: boolean = false;
+	
+	async function verifyEmail(event: any) {
+    event.preventDefault()
+    
+    loading = true
+
+    try {
+      const response = await fetch(`https://api.subvind.com/users/verifyEmail/${user.id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      });
+
+      if (response.ok) {
+				let res = await response.json();
+
+				alert('Verify email code sent successfully.')
+      } else {
+        const errorData = await response.json();
+        alert(errorData.error);
+      }
+    } catch (error) {
+      console.error('Error registering user:', error);
+      alert('An error occurred during submission.');
+    }
+
+    loading = false
+  }
+
+  async function load() {
+    const response = await fetch(`https://api.subvind.com/users/username/${decodedToken.username}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+				'authorization': `Bearer ${localStorage.getItem("access_token")}`
+      }
+    });
+
+    if (response.ok) {
+      user = await response.json();
+    } else {
+      const errorData = await response.json();
+      alert(errorData.error);
+    }
+  }
+
+	onMount(async () => {
+    let accessToken: any = localStorage.getItem('access_token');
+
+    // Decode the JWT
+    decodedToken = jwt_decode(accessToken);
+
+    await load()
+	})
 </script>
 
 <svelte:head>
@@ -11,4 +72,10 @@
 	<h4>Email Verification</h4>
 
   <p>All users must be verified before continuing.</p>
+
+	{#if loading}
+		<button style="margin-left: 1em;" class="waves-effect btn disabled">Loading</button>
+	{:else}
+		<button style="margin-left: 1em;" class="waves-effect yellow black-text lighten-2 btn" on:click={verifyEmail}>send email verification token</button>
+	{/if}
 </div>
