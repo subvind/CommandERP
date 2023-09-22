94041ba24a023c346e8f264706658223ae4acb9d
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-09-22 16:06:05 -0500

    accounts auth

--- src/lib/auth/Login.svelte
+++ src/lib/auth/Login.svelte
@@ -2,49 +2,79 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import jwt_decode from 'jwt-decode';
+    import SelectOrganization from './SelectOrganization.svelte';
 
   let loading: boolean = false;
 	let email = ''
   let password = ''
+  let type = ''
+  let organizationId = ''
 
   async function login(event: any) {
     event.preventDefault()
 
+    if (type === '') return alert('Account type must be defined.')
+    if (organizationId === '') return alert('Organization must be defined.')
     if (email === '') return alert('Email must be defined.')
     if (password === '') return alert('Password must be defined.')
 
     loading = true 
 
     try {
-      const response = await fetch('https://api.subvind.com/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          password
-        }),
-      });
-
-      if (response.ok) {
-        let res = await response.json();
-
-        console.log('access_token', res.access_token)
-
-        // Save the access token to localStorage
-        localStorage.setItem('access_token', res.access_token);
-
-        // Decode the JWT
-        let decodedToken: any = jwt_decode(res.access_token);
-
-        console.log('decoded_token', decodedToken)
-
-        // You can redirect the user to a new page or handle the success scenario in your app
-        window.location.href = `/${decodedToken.username}`
+      if (type === 'root-user') {
+        const response = await fetch('https://api.subvind.com/auth/userLogin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            password
+          }),
+        });
+  
+        if (response.ok) {
+          let res = await response.json();
+  
+          console.log('access_token', res.access_token)
+  
+          // Save the access token to localStorage
+          localStorage.setItem('access_token', res.access_token);
+  
+          // Decode the JWT
+          let decodedToken: any = jwt_decode(res.access_token);
+  
+          console.log('decoded_token', decodedToken)
+  
+          // You can redirect the user to a new page or handle the success scenario in your app
+          window.location.href = `/${decodedToken.username}`
+        } else {
+          const errorData = await response.json();
+          alert(errorData.error);
+        }
       } else {
-        const errorData = await response.json();
-        alert(errorData.error);
+        const response = await fetch('https://api.subvind.com/auth/accountLogin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            password,
+            organizationId
+          }),
+        });
+  
+        if (response.ok) {
+          let res = await response.json();
+          console.log('access_token', res.access_token)
+          localStorage.setItem('access_token', res.access_token);
+          let decodedToken: any = jwt_decode(res.access_token);
+          window.location.href = `${decodedToken.ownername}/${decodedToken.orgname}/accounts/${decodedToken.accountname}`
+        } else {
+          const errorData = await response.json();
+          alert(errorData.error);
+        }
       }
     } catch (error) {
       console.error('Error logging in user:', error);
@@ -56,6 +86,9 @@
 
   onMount(() => {
     M.updateTextFields();
+
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {});
   })
 </script>
 
@@ -71,6 +104,21 @@
   <form class="card" on:submit={(e) => login(e)}>
     <div class="card-content">
       <div class="row">
+        <div class="input-field col s12">
+          <select bind:value={type}>
+            <option value="" disabled selected>Choose your membership</option>
+            <option value="root-user">root user</option>
+            <option value="organization-employee">organization employee</option>
+            <option value="organization-supplier">organization supplier</option>
+            <option value="organization-customer">organization customer</option>
+          </select>
+          <label>Account Type</label>
+        </div>
+        {#if type && type !== 'root-user'}
+          <div class="input-field col s12" style="margin-top: 0;">
+            <SelectOrganization bind:value={organizationId} />
+          </div>
+        {/if}
         <div class="input-field col s12">
           <input id="email" type="email" placeholder="test@test.com" class="validate" bind:value={email}>
           <label for="email">Email</label>
